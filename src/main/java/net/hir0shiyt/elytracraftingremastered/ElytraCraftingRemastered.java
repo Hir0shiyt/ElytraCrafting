1a57e03f9f4ef5b2d3efde867f0bda2c617b5a59
hir0shiyt <hiroshicrackyt@gmail.com>
2024-12-22 17:34:50 +0100

    Initial 1.20.3 version

--- src/main/java/net/hir0shiyt/elytracraftingremastered/ElytraCraftingRemastered.java
+++ src/main/java/net/hir0shiyt/elytracraftingremastered/ElytraCraftingRemastered.java
@@ -1,22 +1,25 @@
-package net.hir0shiyt.ElytraCrafting;
+package net.hir0shiyt.elytracraftingremastered;
 
 import com.mojang.logging.LogUtils;
-import net.hir0shiyt.ElytraCrafting.item.ModItems;
+import net.hir0shiyt.elytracraftingremastered.items.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-@Mod(ElytraMain.MOD_ID)
-public class ElytraMain {
-    public static final String MOD_ID = "elytracrafting";
-    public static final Logger logger = LogUtils.getLogger();
+@Mod(ElytraCraftingRemastered.MODID)
+public class ElytraCraftingRemastered
+{
+    public static final String MODID = "elytracraftingremastered";
+    private static final Logger logger = LogUtils.getLogger();
 
-    public ElytraMain() {
+    public ElytraCraftingRemastered() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
@@ -26,12 +29,18 @@ public class ElytraMain {
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        System.out.println("Build successful" + logger.toString());
+        logger.info("HELLO FROM COMMON SETUP");
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.HALF_ELYTRA);
         }
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        logger.info("HELLO from server starting");
     }
 }
--- src/main/java/net/hir0shiyt/elytracraftingremastered/items/ModItems.java
+++ src/main/java/net/hir0shiyt/elytracraftingremastered/items/ModItems.java
@@ -1,8 +1,7 @@
-package net.hir0shiyt.ElytraCrafting.item;
+package net.hir0shiyt.elytracraftingremastered.items;
 
-import net.hir0shiyt.ElytraCrafting.ElytraMain;
+import net.hir0shiyt.elytracraftingremastered.ElytraCraftingRemastered;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Rarity;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,10 +10,10 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
-            DeferredRegister.create(ForgeRegistries.ITEMS, ElytraMain.MOD_ID);
+            DeferredRegister.create(ForgeRegistries.ITEMS, ElytraCraftingRemastered.MODID);
 
-    public static final RegistryObject<Item> HALF_ELYTRA = ITEMS.register("halfelytra",
-            () -> new Item(new Item.Properties().stacksTo(2).craftRemainder(Items.PHANTOM_MEMBRANE).rarity(Rarity.RARE)));
+    public static final RegistryObject<Item> HALF_ELYTRA = ITEMS.register("half_elytra",
+            () -> new Item(new Item.Properties().stacksTo(2).rarity(Rarity.RARE)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
