d65f4ce13b8e4183edd54bc07ae21b076010b7d3
hir0shiyt <hiroshicrackyt@gmail.com>
2024-12-23 10:52:00 +0100

    Changed item registry

--- src/main/java/net/hir0shiyt/elytracraftingremastered/items/ModItems.java
+++ src/main/java/net/hir0shiyt/elytracraftingremastered/items/ModItems.java
@@ -1,6 +1,9 @@
 package net.hir0shiyt.elytracraftingremastered.items;
 
 import net.hir0shiyt.elytracraftingremastered.ElytraCraftingRemastered;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -13,7 +16,7 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, ElytraCraftingRemastered.MODID);
 
     public static final RegistryObject<Item> HALF_ELYTRA = ITEMS.register("half_elytra",
-            () -> new Item(new Item.Properties().stacksTo(2).rarity(Rarity.RARE)));
+            () -> new Item(new Item.Properties().stacksTo(2).rarity(Rarity.RARE).setId(ResourceKey.create(Registries.ITEM, ResourceLocation.parse("elytracraftingremastered:half_elytra")))));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
